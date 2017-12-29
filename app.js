f6ffba3e98e5edacf5015347410d1b20c90996e3
zhangjianxin <zhangjx@upay360.com>
2017-12-29 10:21:21 +0800

    调整logger配置

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var log4js = require('log4js');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var crypto = require("crypto");
-var logger = require('./logger')();
 
 var index = require('./routes/index');
 
@@ -20,7 +19,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(log4js.connectLogger(require('./logger')(), {level: 'auto'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
