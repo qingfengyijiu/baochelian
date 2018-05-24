d8ab64f8cb69596cc1a0a23915c2228de908aab4
zhangjianxin <zhangjx@upay360.com>
2018-05-24 13:52:07 +0800

    介绍页介入微信分享

--- src/js/views/draw/Introduction.jsx
+++ src/js/views/draw/Introduction.jsx
@@ -1,6 +1,84 @@
 import React, {Component} from 'react'
+import ws from "../../lib/ws";
 
 export default class extends Component {
+
+	componentDidMount() {
+		const timestamp = new Date().getTime().toString().slice(0, 10)
+		const nonceStr = "testbaochelian"
+		const appid = "wx3e98278c327dfef2"
+		const debug = false
+		const jsApiList = ["onMenuShareTimeline", "onMenuShareAppMessage"]
+		const _this = this
+		ws.get({
+			url: "/api/wechat/jsapi_signature",
+			data: {
+				timestamp: timestamp,
+				nonceStr: nonceStr,
+				url: window.location.href.replace(/#.*/g, "")
+			}
+		}).then(response => {
+			wx && wx.config({
+				debug: debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+				appId: appid, // 必填，公众号的唯一标识
+				timestamp: timestamp, // 必填，生成签名的时间戳
+				nonceStr: nonceStr, // 必填，生成签名的随机串
+				signature: response.data.signature,// 必填，签名
+				jsApiList: jsApiList // 必填，需要使用的JS接口列表
+			});
+		})
+		wx && wx.ready(function() {
+			wx.onMenuShareTimeline({
+				title: '保车连老板疯了，iPhone X免费送',
+				link: location.origin + '/draw/introduction',
+				imgUrl: location.origin + '/images/draw/head.png',
+				success: function() {
+					ws.post({
+						url: "/api/coupon/custom"
+					}).then(response => {
+						if(response.code === 0) {
+							_this.setState({
+								showCouponDialog: true,
+								parPrice: response.data.parPrice
+							})
+						} else {
+							alert(response.message)
+						}
+					})
+				},
+				fail: function() {},
+				complete: function() {},
+				cancel: function() {},
+				trigger: function() {}
+			})
+			wx.onMenuShareAppMessage({
+				title: '保车连老板疯了，iPhone X免费送',
+				desc: '保车连老板疯了，iPhone X免费送',
+				link: location.origin + '/draw/introduction',
+				imgUrl: location.origin + '/images/draw/head.png',
+				success: function() {
+					ws.post({
+						url: "/api/coupon/custom"
+					}).then(response => {
+						if(response.code === 0) {
+							_this.setState({
+								showCouponDialog: true,
+								parPrice: response.data.parPrice
+							})
+						} else {
+							alert(response.message)
+						}
+					})
+				},
+				fail: function() {},
+				complete: function() {},
+				cancel: function() {},
+				trigger: function() {}
+			})
+		})
+
+	}
+
 	render() {
 		return (
 			<div className="page-introduction">
